5c9f604c821ade932e96f981632fd8d34cf0fa27
wanghao0730 <1149243584@qq.com>
2020-06-21 16:51:38 +0800

    爬取实时播报数据

--- spider.js
+++ spider.js
@@ -19,14 +19,19 @@ superagent
     const $getListByCountryTypeService2true = $('#getListByCountryTypeService2true').html();
     //todo 国内疫情
     const $getAreaStat = $('#getAreaStat').html();
+    //todo 实时播报
+    const $getTimelineService1 = $('#getTimelineService1').html();
+    console.log($getTimelineService1);
     //! 存储国外疫情的数据 使用
     let worldData = {};
     //! 存储中国内陆的数据
     let areaData = {};
+    //! 实时播报数据
+    let timeService = {}
     eval($getListByCountryTypeService2true.replace(/window/g, 'worldData'));
-    eval($getAreaStat.replace(/window/g, 'areaData'));
-    console.log($getListByCountryTypeService2true);
-    console.log($getAreaStat);
+    eval($getAreaStat.replace(/window/g, 'areaData'))
+    eval($getTimelineService1.replace(/window/g, 'timeService'));
+    // console.log($getListByCountryTypeService2true);
     //! 写入到存储世界疫情数据的json中
     fs.writeFile(path.join(__dirname, 'data/world-data.json'), JSON.stringify(worldData), (err) => {
       if (err) {
@@ -36,6 +41,13 @@ superagent
     })
     //! 写入到存储国内疫情数据的json中
     fs.writeFile(path.join(__dirname, 'data/area-data.json'), JSON.stringify(areaData), (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log('数据写入成功');
+    })
+    //! 将实时播报数据写入 json
+    fs.writeFile(path.join(__dirname, 'data/time-service.json'), JSON.stringify(timeService), (err) => {
       if (err) {
         throw err;
       }
